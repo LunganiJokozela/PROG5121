b43733b04dd1f9d4febc1d92fb560c90e76a6a2a
LunganiJokozela <165771422+LunganiJokozela@users.noreply.github.com>
2024-04-16 10:40:30 +0200

    Add files via upload

---
+++ LunganiJokozelaJava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lungani.jokozela.java;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LunganiJokozelaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String fullName = "Lungani Jokozela";
+        String emailAddress = "jokozelalungani17@icloud.com";
+        String phoneNumber = "0692829865";
+
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Email Address: " + emailAddress);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+    
+}
